ea20a8e9ee1b1544055eeeece0f77ae670659f0b
Charlie C <snailrose@gmail.com>
2023-04-15 06:05:57 -0600

    Update

--- CMake/Configure.cmake
+++ CMake/Configure.cmake
@@ -29,19 +29,19 @@ include(ExternalTarget)
 
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
-option(Template_BUILD_TEST          "Build the unit test program." ON)
-option(Template_AUTO_RUN_TEST       "Automatically run the test program." ON)
-option(Template_USE_STATIC_RUNTIME  "Build with the MultiThreaded(Debug) runtime library." ON)
+option(Tbb_BUILD_TEST          "Build the unit test program." ON)
+option(Tbb_AUTO_RUN_TEST       "Automatically run the test program." ON)
+option(Tbb_USE_STATIC_RUNTIME  "Build with the MultiThreaded(Debug) runtime library." ON)
 
-if (Template_USE_STATIC_RUNTIME)
+if (Tbb_USE_STATIC_RUNTIME)
     set_static_runtime()
 else()
     set_dynamic_runtime()
 endif()
 
 
-configure_gtest(${Template_SOURCE_DIR}/Test/googletest 
-                ${Template_SOURCE_DIR}/Test/googletest/googletest/include)
+configure_gtest(${Tbb_SOURCE_DIR}/Test/googletest 
+                ${Tbb_SOURCE_DIR}/Test/googletest/googletest/include)
 
 
 set(Configure_SUCCEEDED TRUE)
