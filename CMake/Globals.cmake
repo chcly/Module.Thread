d3783fbd1315c2d71ff74cff68eab65ce180fdab
Charlie C <snailrose@gmail.com>
2023-05-27 20:36:15 -0600

    Update

--- CMake/Globals.cmake
+++ CMake/Globals.cmake
@@ -1,7 +1,11 @@
 option(Thread_USE_TBB "Build with Intell Thread Building Blocks." OFF)
 
 if (Thread_USE_TBB)
-    include(${Thread_SOURCE_DIR}/CMake/findTBB.cmake)
+    if (Thread_DIRECTORY)
+        include(${Thread_DIRECTORY}/CMake/FindTBB.cmake)
+    elseif(Thread_SOURCE_DIR)
+        include(${Thread_SOURCE_DIR}/CMake/FindTBB.cmake)
+    endif()    
     # Note: Dependent on where Thread_INCLUDE and Thread_LIBRARY are defined,
     # Standalone: Toplevel CMakeLists.txt
     # SubModule:  DefineExternalTargetEx -> include this file...
@@ -9,20 +13,20 @@ if (Thread_USE_TBB)
         set(Thread_DEFINITIONS -DUSE_ONEAPI)
         
         if (ExternalTarget_LOG)
-            message("APPENDING ${TBB_SDK_INCLUDE} -> ${Thread_INCLUDE}" )
+            message(STATUS "APPENDING ${TBB_SDK_INCLUDE} -> ${Thread_INCLUDE}" )
         endif()
 
         list(APPEND Thread_INCLUDE ${TBB_SDK_INCLUDE})
 
         if (ExternalTarget_LOG)
-            message("APPENDING TBB -> ${Thread_LIBRARY}" )
+            message(STATUS "APPENDING TBB -> ${Thread_LIBRARY}" )
         endif()
 
         list(APPEND Thread_LIBRARY TBB)
 
 
         if (ExternalTarget_LOG)
-            message("APPENDING Global definitions ${Thread_DEFINITIONS}" )
+            message(STATUS "APPENDING Global definitions ${Thread_DEFINITIONS}" )
         endif()
 
         add_definitions(${Thread_DEFINITIONS})
